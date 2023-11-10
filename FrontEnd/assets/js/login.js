b09976d85e2045ddeccdac4a27b4524d4747c9b0
Laurie-anne Doré <lau.adore@gmail.com>
2023-11-09 20:43:09 -0400

    login page &  Modal v1

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,2 +1,38 @@
 "use strict";
 
+// ********** CONSTANTS ********** //
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const submit = document.getElementById("submit");
+
+// ********** FUNCTIONS ********** //
+function login(event) {
+    event.preventDefault()
+
+    const logData = {
+        email: email.value,
+        password: password.value
+    };
+
+    fetch("http://localhost:5678/api/users/login",
+        {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(logData),
+        })
+        .then(resp => {
+            if (resp.ok) {
+                resp.json().then(data => {
+                    localStorage.setItem("userId", data.userId);
+                    localStorage.setItem("token", data.token);
+                    location.href = "index.html";
+                })
+            } else {
+                alert("Identifiant ou mot de passe incorrect !");
+            }
+        })
+}
+
+
+// ********** MAIN ********** //
+submit.addEventListener("click", login);
--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 // ********** CONSTANTS ********** //
+const body = document.querySelector("body");
+const portfolioSection = document.getElementById("portfolio");
 const portfolioContainer = document.querySelector(".direction");
 const galleryContainer = document.querySelector(".gallery");
 const filtersContainer = document.createElement("ul");
@@ -9,13 +11,10 @@ const filtersContainer = document.createElement("ul");
 const projectId = document.getElementById("1");
 
 // ********** VARIABLES ********** //
-
 let projects = [];
 let categories = [];
 
 // ********** FUNCTIONS ********** //
-
-
 async function fetchWorks() {
     const url = "http://localhost:5678/api/works"
     const response = await fetch(url);
@@ -50,7 +49,7 @@ function addProjects(projects) {
 
 
 // Creation des filtres 
-function addAllFilter() {
+function addFilterAll() {
     const filterAllContnair = document.createElement("li")
     const filterAllElement = document.createElement("button");
     filterAllElement.innerText = "Tous";
@@ -99,74 +98,50 @@ function addFiltersClass() {
     }
 }
 
-function removeProjects1() {
-    const projectsDip = document.querySelectorAll(".projects")
-    console.log(projectsDip)
+function ObjectsFilter() {
+    const projectsDip = document.querySelectorAll(".projects");
 
     for (const projectDip of projectsDip) {
-        switch (projectDip.id) {
-            case "1":
-                projectDip.classList.add("displayOptionY");
-                projectDip.classList.remove("displayOptionN");
-                break;
-
-            case "2":
-                projectDip.classList.add("displayOptionN");
-                break
-
-            case "3":
-                projectDip.classList.add("displayOptionN");
-                break
+        if (projectDip.id === "1") {
+            projectDip.classList.add("displayOptionY");
+            projectDip.classList.remove("displayOptionN");
+        }
+        else {
+            projectDip.classList.add("displayOptionN");
         }
     }
 }
 
-function removeProjects2() {
-    const projectsDip = document.querySelectorAll(".projects")
-    console.log(projectsDip)
+function ApptFilter() {
+    const projectsDip = document.querySelectorAll(".projects");
 
     for (const projectDip of projectsDip) {
-        switch (projectDip.id) {
-            case "1":
-                projectDip.classList.add("displayOptionN");
-                break
-
-            case "2":
-                projectDip.classList.add("displayOptionY");
-                projectDip.classList.remove("displayOptionN");
-                break
-
-            case "3":
-                projectDip.classList.add("displayOptionN");
-                break
+        if (projectDip.id === "2") {
+            projectDip.classList.add("displayOptionY");
+            projectDip.classList.remove("displayOptionN");
+        }
+        else {
+            projectDip.classList.add("displayOptionN");
         }
     }
 }
 
-function removeProjects3() {
-    const projectsDip = document.querySelectorAll(".projects")
-    console.log(projectsDip)
+function HotelAndResFilter() {
+    const projectsDip = document.querySelectorAll(".projects");
 
     for (const projectDip of projectsDip) {
-        switch (projectDip.id) {
-            case "1":
-                projectDip.classList.add("displayOptionN");
-                break
-
-            case "2":
-                projectDip.classList.add("displayOptionN");
-                break
-
-            case "3":
-                projectDip.classList.add("displayOptionY");
-                projectDip.classList.remove("displayOptionN");
-                break
+        if (projectDip.id === "3") {
+            projectDip.classList.add("displayOptionY");
+            projectDip.classList.remove("displayOptionN");
+        }
+        else {
+            projectDip.classList.add("displayOptionN");
         }
     }
 }
 
-function paramAllFilters() {
-    const projectsDip = document.querySelectorAll(".projects")
+function paramAllFilter() {
+    const projectsDip = document.querySelectorAll(".projects");
     for (const projectDip of projectsDip) {
         projectDip.classList.add("displayOptionN");
         projectDip.classList.remove("displayOptionN");
@@ -181,16 +156,83 @@ function paramFilters() {
     const allfilters = document.querySelector(".allfilters");
 
 
-    objectsFilter.addEventListener("click", removeProjects1);
+    objectsFilter.addEventListener("click", ObjectsFilter);
 
-    apartmentsFilter.addEventListener("click", removeProjects2);
+    apartmentsFilter.addEventListener("click", ApptFilter);
 
-    hotelsRestauFilter.addEventListener("click", removeProjects3);
+    hotelsRestauFilter.addEventListener("click", HotelAndResFilter);
 
-    allfilters.addEventListener("click", paramAllFilters);
+    allfilters.addEventListener("click", paramAllFilter);
 
 }
 
+
+// Creation de la modal
+function displayModal() {
+    logElementParam();
+    addProjectEdit();
+    addBanner();
+        
+}
+
+function addBanner() {
+    const banner = document.createElement("div");
+    banner.classList.add("banner");
+
+    const bannerElementContainer = document.createElement("a");
+
+    const editContainer = document.createElement("figure");
+    const editIcon = document.createElement("i");
+    editIcon.classList.add("fa-solid", "fa-pen-to-square");
+
+    const bannerTxt = document.createElement("figcaption");
+    bannerTxt.innerText = "Mode édition";
+
+    body.insertBefore(banner, body.firstChild);
+    banner.appendChild(bannerElementContainer);
+    bannerElementContainer.appendChild(editContainer);
+
+    editContainer.appendChild(editIcon);
+    editContainer.appendChild(bannerTxt);
+}
+
+function addProjectEdit() {
+    const editContainer = document.createElement("a");
+    editContainer.classList.add("edit-container");
+    
+    const editIconContainer = document.createElement("figure");
+
+    const editIcon = document.createElement("i");
+    editIcon.classList.add("fa-solid", "fa-pen-to-square");
+
+    const editTxt = document.createElement("figcaption");
+    editTxt.innerText = "Modifier";
+
+    portfolioSection.insertBefore(editContainer, portfolioContainer);
+    editContainer.appendChild(editIconContainer);
+    editIconContainer.appendChild(editIcon);
+    editIconContainer.appendChild(editTxt);
+}
+    
+function logElementParam() {
+    const authElt = document.querySelector('[href="login.html"]');
+    authElt.innerText = "logout";
+    authElt.setAttribute("href", "#");
+    authElt.addEventListener("click", (event) => {
+        event.preventDefault();
+        localStorage.removeItem("token") && localStorage.removeItem("userId");
+        authElt.innerText = "login";
+        authElt.setAttribute("href", "login.html");
+        bodyBanner.remove();
+        editBanner.remove();
+        publishChanges.remove();
+        editImg.remove();
+        editArticle.remove();
+        editGallery.remove();
+        alert("Vous êtes déconnecté");
+    });
+
+}
 
 
 // ********** MAIN ********** //
@@ -199,11 +241,15 @@ portfolioContainer.appendChild(filtersContainer);
 fetchWorks()
     .then(() => {
         addProjects(projects);
+
+        if (localStorage.getItem("token")) {
+            displayModal();
+        }
     })
 
 fetchCategories()
     .then(() => {
-        addAllFilter();
+        addFilterAll();
         addFilters(categories);
         addFiltersClass();
         paramFilters();
